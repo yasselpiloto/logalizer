8849a4aa3195101275ef24b0dd2cd3048b3ec7e4
Jazz <yassel_piloto@ultimatesoftware.com>
2015-04-29 23:03:43 -0400

    experiment with key bindings

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ import sublime_plugin
 
 class LogalyzeEventListener(sublime_plugin.EventListener):
   def on_query_context(self, view, key, operator, operand, match_all):
-    print("key: %s" % key)
+
+    if not key.startswith('logalizer-key.'):
+      return None
+
+      print("apretaron algo")
+
+      return False
+
+class RunLogalyzerAction(sublime_plugin.TextCommand):
+  def run(self, edit, action=None, **kw):
+    print("apretaron argooooooooo")
